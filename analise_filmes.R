285ba19a1a7f7c883ae7b14aa5d338bf0a4aaed8
gabriel-gonzales <glg.gabrielgonzale@gmail.com>
2024-05-20 23:14:40 -0300

    código ainda para ser corrigido

--- analise_filmes.R
+++ analise_filmes.R
@@ -1,8 +1,9 @@
 # Carregar pacotes necessários
 library(dplyr)
+library(readr)
 
 # Carregar dados do IMDb
-data <- readr::read_tsv("title.basics.tsv.gz")
+data <- read_tsv("title.basics.tsv.gz")
 
 # Limpeza de dados
 
@@ -17,14 +18,10 @@ filmes <- filmes %>%
 # 3. Selecionar colunas relevantes e converter tipos de dados
 filmes <- filmes %>% 
   select(primaryTitle, startYear, runtimeMinutes, genres) %>%
-  mutate(startYear = as.numeric(startYear),
-         runtimeMinutes = as.numeric(runtimeMinutes))
+  mutate(startYear = as.numeric(startYear)) %>%
+  filter(!is.na(startYear))  # Remover linhas com valores NA na coluna startYear
 
 # Verificar se há problemas de conversão
-if (any(is.na(filmes$startYear))) {
-  # Imprimir linhas com problemas
-  print(filmes[is.na(filmes$startYear), ])
-}
 if (any(is.na(filmes$runtimeMinutes))) {
   # Imprimir linhas com problemas
   print(filmes[is.na(filmes$runtimeMinutes), ])
@@ -32,12 +29,18 @@ if (any(is.na(filmes$runtimeMinutes))) {
 
 # Análise de dados
 
-
 # Visualização da distribuição do ano de lançamento
 hist(filmes$startYear, main = "Distribuição do Ano de Lançamento", xlab = "Ano de Lançamento")
 
+# Verificar se há problemas de parsing
+problems <- problems(read_tsv("title.basics.tsv.gz"))
+
 # Visualização da distribuição da duração dos filmes
-hist(filmes$runtimeMinutes, main = "Distribuição da Duração dos Filmes", xlab = "Duração (minutos)")
+if (all(is.na(filmes$runtimeMinutes)) | all(!is.na(as.numeric(filmes$runtimeMinutes)))) {
+  hist(as.numeric(filmes$runtimeMinutes), main = "Distribuição da Duração dos Filmes", xlab = "Duração (minutos)")
+} else {
+  print("Existem valores não numéricos em runtimeMinutes após a limpeza dos dados.")
+}
 
 # Salvar as visualizações em um diretório de plots
 png("plots/distribuicao_ano_lancamento.png")
@@ -45,7 +48,11 @@ hist(filmes$startYear, main = "Distribuição do Ano de Lançamento", xlab = "An
 dev.off()
 
 png("plots/distribuicao_duracao_filmes.png")
-hist(filmes$runtimeMinutes, main = "Distribuição da Duração dos Filmes", xlab = "Duração (minutos)")
+if (all(is.na(filmes$runtimeMinutes)) | all(!is.na(as.numeric(filmes$runtimeMinutes)))) {
+  hist(as.numeric(filmes$runtimeMinutes), main = "Distribuição da Duração dos Filmes", xlab = "Duração (minutos)")
+} else {
+  print("Existem valores não numéricos em runtimeMinutes após a limpeza dos dados.")
+}
 dev.off()
 
 # Salvar os dados limpos em um arquivo CSV
